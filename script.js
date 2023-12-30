fd797780a0176033be59fbb8a371240c1449dbfd
Bikash <130203325+Bikashbudhathoki@users.noreply.github.com>
2023-12-31 03:42:30 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+function changeColor() {
+    document.getElementById("header").style.color = "orange";
+}
+
+function changeColorBack() {
+    document.getElementById("header").style.color = "white";
+}
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+var slideIndex = 0;
+
+function automaticSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    if (slideIndex < 1) {
+        slideIndex = slides.length;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(automaticSlides, 2000);
+}
+
+automaticSlides();
